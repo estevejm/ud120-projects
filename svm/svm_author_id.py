3dbe001acf1563f6cef0738d3a19b894e3e1a5dc
Esteve Julià <estevejuliamelis@gmail.com>
2016-02-15 00:51:32 +0100

    SVM mini-project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -13,7 +13,6 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
-
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
@@ -25,6 +24,32 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+# features_train = features_train[:len(features_train)/100] 
+# labels_train = labels_train[:len(labels_train)/100] 
+
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+classifier = SVC(kernel='rbf', C=10000.0)
+
+t0 = time()
+classifier.fit(features_train, labels_train)
+print "Training time: %s s" % round(time()-t0, 3)
+
+t0 = time()
+predictions = classifier.predict(features_test)
+print "Prediction time: %s s" % round(time()-t0, 3)
+
+accuracy = accuracy_score(predictions, labels_test)
+
+print "Predicitions: %s" % predictions
+print "Accuracy: %s" % accuracy
+
+unique, counts = np.unique(predictions, return_counts=True)
+
+print "Emails from Sara: %s" % counts[0]
+print "Emails from Chris: %s" % counts[1]
 #########################################################
 
 
