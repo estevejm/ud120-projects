98f9bc9b6befe315ccfe4016acdae8a9afbc00bd
Esteve Julià <estevejuliamelis@gmail.com>
2016-02-15 21:39:04 +0100

    Decission trees

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -25,7 +25,29 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
+classifier = DecisionTreeClassifier(min_samples_split=40)
+
+t0 = time()
+classifier.fit(features_train, labels_train)
+print "Training time: %s s" % round(time()-t0, 3)
+
+t0 = time()
+predictions = classifier.predict(features_test)
+print "Prediction time: %s s" % round(time()-t0, 3)
+
+accuracy = accuracy_score(predictions, labels_test)
+
+print "Predicitions: %s" % predictions
+print "Accuracy: %s" % accuracy
+
+unique, counts = np.unique(predictions, return_counts=True)
+
+print "Emails from Sara: %s" % counts[0]
+print "Emails from Chris: %s" % counts[1]
 #########################################################
 
 
